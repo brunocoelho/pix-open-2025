d43d4eace075603af34d047d63a60ece760d19cd
Bruno Coelho <bcs.cin@gmail.com>
2025-11-11 11:33:29 -0300

    add credentials do enable CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,11 +5,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    credentials: true,
   });
-  // app.enableCors({
-  //   origin: 'http://localhost:3000',
-  //   credentials: true,
-  // });
   await app.listen(process.env.PORT ?? 3001);
 }
 

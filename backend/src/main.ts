e3e14f6fd7399aa6598633b14078b9fc45e2eb8f
Bruno Coelho <bcs.cin@gmail.com>
2025-11-11 09:33:00 -0300

    add CORS URLS to BE

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  });
   // app.enableCors({
   //   origin: 'http://localhost:3000',
   //   credentials: true,
